64a3a3a4b9ca7f4af24f16e197c726635d417e22
Maxim <therikomero@gmail.com>
2023-10-29 22:56:00 +0700

    minor style fixes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Main {
+
+    private Main() { }
+
     public static void main(String[] args) {
 
     }
--- src/main/java/org/example/MainPage.java
+++ src/main/java/org/example/MainPage.java
@@ -1,12 +1,12 @@
 package org.example;
 
 public class MainPage implements WebPage {
-    User authenticateUser(String username, String password, DataBase db){
+    User authenticateUser(String username, String password, DataBase db) {
         User user = new DefaultUser();
         return user.authenticate(username, password, db);
     }
 
-    boolean isAllowed(User user){
+    boolean isAllowed(User user) {
         return user != null;
     }
 
