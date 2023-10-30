13313334b81018adb0b1e357b13cf40666bc4623
Maxim <therikomero@gmail.com>
2023-10-31 00:22:48 +0700

    Fix style issues

--- src/main/java/org/example/DataBase.java
+++ src/main/java/org/example/DataBase.java
@@ -3,14 +3,19 @@ package org.example;
 import java.util.HashMap;
 
 public class DataBase {
-    private static final HashMap<String, User> db = new HashMap<>();
+
+    private DataBase() {
+
+    }
+
+    private static final HashMap<String, User> DB = new HashMap<>();
 
     public static void addUser(User user) {
-        db.put(user.getUsername(), user);
+        DB.put(user.getUsername(), user);
     }
 
     public static User getUserByUsernameAndPassword(String username, String password) {
-        User user = db.get(username);
+        User user = DB.get(username);
         return user.getPassword().equals(password) ? user : null;
     }
 }
